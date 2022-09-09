12f91e138fe198fd43872724b7fbc819ebc5e944
Sara Posada <sara.posadar@upb.edu.co>
2022-09-08 23:18:10 -0500

    autograding solucionado

--- problem/program.asm
+++ problem/program.asm
@@ -3,116 +3,124 @@
 // sara.posadar@upb.edu.co
 
 (INICIO)
-	@KBD
-	D=M
-	@84
-	D=D-A
-	@VERTICAL
-	D;JEQ
-	@KBD
-	D=M
-	@67
-	D=D-A
-	@BORRAR
-	D;JEQ
+		@KBD
+		D=M
+		@84
+		D=D-A
 
-	@INICIO
-	0;JMP
+		@VERTICAL
+		D;JEQ
 
+		@KBD
+		D=M
+		@67
+		D=D-A
+
+		@SETCL
+		D;JEQ
+
+		@INICIO
+		0;JMP
+
+(HORIZONTAL)
+		@20480
+		D=A
+		@coord
+		M=D
+		@32
+		D=A
+		@cont
+		M=D
+		@1
+		D=A
+		@salto
+		M=D
+		@color
+		M=-1
+		@if
+		M=1
+
+		@RECT
+		0;JMP
 
 (VERTICAL)
-	@16400 
-	D=A 
-	@coord
-	M=D
-	@256 
-	D=A
-	@cont
-	M=D
-	@32
-	D=A
-	@salto
-	M=D
-	@256
-	D=A
-	@color
-	M=D         
-	@coord
-	D=M
-	@pscreenprogram.asm
-	M=D
-	@color2
-	M=-1	
-	@coord         
-	D=M
-	@pscreen
-	M=D
-	
-	@LIMPIAR
-	0;JMP
+		@16400 
+		D=A 
+		@coord
+		M=D
+		@256 
+		D=A
+		@cont
+		M=D
+		@32
+		D=A
+		@salto
+		M=D
+		@256
+		D=A
+		@color
+		M=D
+		@if
+		M=0
+
+		@RECT
+		0;JMP
+(SETCL)
+        @16384
+        D=A
+        @coord
+        M=D 
+        @8192
+        D=A
+        @cont
+        M=D
+        @1
+        D=A 
+        @salto
+        M=D
+        @color
+        M=0
+
+        @RECT
+        0;JMP
+
+(RECT)
+        @coord
+        D=M
+        @pscreen
+        M=D
 
 (PINTAR)
-	@cont
-	D=M
-	@HORIZONTAL
-	D;JEQ
-	@color
-	D=M
-	@pscreen
-	A=M
-	M=D
-	@cont
-	M=M-1
-	@salto
-	D=M
-	@pscreen
-	M=M+D
-	
-	@PINTAR
-	0;JMP
+		@cont
+        D=M 
 
-(BORRAR)
-	@16384
-	D=A
-	@coord        
-	M=D
-	@8192
-	D=A
-	@cont2
-	M=D
-	@1
-	D=A
-	@salto2
-	M=D
-	@color2
-	M=0
-	@coord         
-	D=M
-	@pscreen
-	M=D
-	
-	@LIMPIAR
-	0;JMP
+        @FINAL
+        D;JEQ
 
-(LIMPIAR)
-	@cont2program
-	D=M
-	@pscreen
-	A=M
-	M=D
-	@cont2
-	M=M-1
-	@salto2
-	D=M
-	@pscreen
-	M=M+D
-	
-	@LIMPIAR
-	0;JMP
+        @color
+        D=M 
+        @pscreen
+        A=M 
+        M=D
+        @cont
+        M=M-1
+        @salto
+        D=M
+        @pscreen
+        M=M+D
+
+        @PINTAR
+        0;JMP
 
 (FINAL)
-	@INICIO
-	0;JMP
+        @if
+        D=M 
+		
+        @HORIZONTAL
+        D;JEQ  
+		
+        @INICIO
+        0;JMP
 
 		
 	
